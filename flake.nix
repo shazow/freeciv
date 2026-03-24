0ba8d23bf2e01b312c4a26592aea52887b22746f
Andrey Petrov <shazow@gmail.com>
2026-03-24 17:01:37 -0400

    Fix flake for local nix build

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Build Freeciv from local source";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in {
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.freeciv.overrideAttrs (old: {
+            pname = "freeciv";
+            version = "source";
+            src = self;
+
+            nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [
+              pkgs.autoreconfHook
+              pkgs.python3
+            ];
+
+            preConfigure = (old.preConfigure or "") + ''
+              mkdir -p build
+              cd build
+            '';
+
+            configureScript = "../configure";
+
+            configureFlags = (old.configureFlags or [ ]) ++ [
+              "--enable-ack-legacy"
+            ];
+          });
+        });
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.mkShell {
+            inputsFrom = [ self.packages.${system}.default ];
+          };
+        });
+    };
+}
